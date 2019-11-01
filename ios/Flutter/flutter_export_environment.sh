544de6f132f9d55d74895f4aab907bde23b2cbc4
KazToozs <cris.hobson@hotmail.com>
2019-11-01 13:02:43 +0100

    version fix

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\crist.LAPTOP-9SPOS1EL\Documents\FlutterMLApp"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/home/kaztoozs/.dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/kaztoozs/Workspace/Siraj/10-IoT/Homework/FlutterMLApp"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/kaztoozs/.dev/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- lib/model/payments_repository.dart
+++ lib/model/payments_repository.dart
@@ -6,9 +6,9 @@ class PaymentsRepository {
   static Random _random = Random();
   static String actuallyMakeTheCharge(String nonce) {
     // secrets here!
-    if (_random.nextBool()) {
+    /* if (_random.nextBool()) {
       return 'Your credit card was declined';
-    }
+    } */
     // actually make the charge here
     return 'Success!';
   }
